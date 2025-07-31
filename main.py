ea7267c726b6546ca343137a3edea3eea40398ed
shivakumar <shiva.14k@gmail.com>
2025-07-31 11:05:41 +0530

    updated mainfile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # codebasics ML course: codebasics.io, all rights reserverd
 
 import streamlit as st
-from predict_help import predict
+from prediction_helper import predict
 
 # Define the page layout
 st.title('Health Insurance Cost Predictor')
